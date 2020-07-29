b8fa2786cc02af50b7120783aa13e9d701540103
Tarush Gupta <tgupta22@uic.edu>
2020-07-29 06:52:07 +0000

    Test

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 let projects = [
-    {name: "project 1",
-     url_repo: ""   }
+    {title: "project 1",
+     subtitle: "a really cool project",
+     url_repo: "https://github.com/username/reponame",
+     url_project: "https://username.github.io/reponame/",
+     technologies: "html,css,rwd,etc",
+     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sapien massa, sagittis eu purus ut, aliquet tincidunt enim. Vestibulum eu sem sit amet lorem ullamcorper dictum. Vivamus lacinia urna sit amet augue mattis, a consectetur ligula facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+     date: "2019 November"
+    }
     
     
 ]
