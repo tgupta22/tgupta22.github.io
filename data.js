2f9e42716d37534702c904d1075a06faaf31191b
Tarush Gupta <tgupta22@uic.edu>
2020-08-04 06:11:36 +0000

    Test2

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 let projects = [
     {title: "project 1",
-     subtitle: "a really cool project",
-     url_repo: "https://github.com/tgupta22",
-     url_project: "https://username.github.io/reponame/",
+     subtitle: "CTA Train & Bus Schedule (Progressive Web App)",
+     url_repo: "https://github.com/tgupta22/it202-big-project",
+     url_project: "https://tgupta22.github.io/it202-big-project/",
      technologies: "html,css,rwd,etc",
-     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sapien massa, sagittis eu purus ut, aliquet tincidunt enim. Vestibulum eu sem sit amet lorem ullamcorper dictum. Vivamus lacinia urna sit amet augue mattis, a consectetur ligula facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+     description: "Built a Progressive Web App that allows the users to check CTA Train & Bus Schedule.  Used Bootstrap or Material Components for the Web for the user UI. Incorporated Google Maps and markers for each record. Cached user history, favorite places etc; also allowed user to use the application.",
      date: "2019 November"
     }
     
